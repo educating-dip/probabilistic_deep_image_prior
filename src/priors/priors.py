f8903bd89357d84d7d1a889f50a0c48fc9152c79
rb876 <riccardo.barbano.19@ucl.ac.uk>
2021-09-23 11:05:46 +0100

    added cholesky decomposition & fixed mean & renamed nn.Parameters

--- src/priors/priors.py
+++ src/priors/priors.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
+import numpy as np
 import itertools
+from torch import linalg
 from torch.distributions.multivariate_normal import MultivariateNormal
-
-
 class RadialBasisFuncCov(nn.Module):
 
     def __init__(
@@ -15,53 +15,59 @@ class RadialBasisFuncCov(nn.Module):
         ):
 
         super(RadialBasisFuncCov, self).__init__()
-        self.dist_mat = self.compute_dist_matrix(kernel_size, dist_func)
-        self.lengthscale = nn.Parameter(torch.Tensor(1))
-        self.precision = nn.Parameter(torch.Tensor(1))
+        self.kernel_size = kernel_size
+        self.dist_mat = self.compute_dist_matrix(dist_func)
+        self.log_lengthscale = nn.Parameter(torch.Tensor(1))
+        self.log_precision = nn.Parameter(torch.Tensor(1))
         self._init_parameters(lengthscale_init, precision_init)
 
     def _init_parameters(self, lengthscale_init, precision_init):
-        nn.init.constant_(self.lengthscale, lengthscale_init)
-        nn.init.constant_(self.precision, precision_init)
+        nn.init.constant_(self.log_lengthscale,
+                          np.log(lengthscale_init))
+        nn.init.constant_(self.log_precision, np.log(precision_init))
 
-    def cov_mat(self):
-        lengthscale = torch.exp(self.lengthscale) + 1e-6
-        precision = torch.exp(self.precision) + 1e-6
-        return precision * torch.exp(-self.dist_mat / lengthscale)
+    def cov_mat(self, return_cholesky=True):
+        lengthscale = torch.exp(self.log_lengthscale) + 1e-6
+        precision = torch.exp(self.log_precision) + 1e-6
+        cov_mat = precision * torch.exp(-self.dist_mat / lengthscale)
+        return (linalg.cholesky(cov_mat) if return_cholesky else cov_mat)
 
-    def compute_dist_matrix(self, kernel_size, dist_func):
+    def compute_dist_matrix(self, dist_func):
         coords = [torch.as_tensor([i, j], dtype=torch.float32) for i in
-                  range(kernel_size) for j in range(kernel_size)]
+                  range(self.kernel_size) for j in
+                  range(self.kernel_size)]
         combs = [[el_1, el_2] for el_1 in coords for el_2 in coords]
         dist_mat = torch.as_tensor([dist_func(el1 - el2) for (el1,
                                    el2) in combs], dtype=torch.float32)
-        return dist_mat.view(kernel_size ** 2, kernel_size ** 2)
+        return dist_mat.view(self.kernel_size ** 2, self.kernel_size
+                             ** 2)
 
 
 class MultiVariateGaussianPrior(nn.Module):
 
-    def __init__(self, mean, covariance_function):
+    def __init__(self, covariance_function):
         super(MultiVariateGaussianPrior, self).__init__()
         self.cov = covariance_function
-        self.mean = mean
 
     def sample(self, shape):
-        m = MultivariateNormal(loc=self.mean,
-                               covariance_matrix=self.cov.cov_mat())
-        return m.rsample(sample_shape=shape)
 
+        cov = self.cov.cov_mat(return_cholesky=True)
+        mean = torch.zeros(self.cov.kernel_size ** 2)
+        m = MultivariateNormal(loc=mean, scale_tril=cov)
+        params_shape = shape + [self.cov.kernel_size,
+                                self.cov.kernel_size]
+        return m.rsample(sample_shape=shape).view(params_shape)
 
 if __name__ == '__main__':
 
-    dist_func = lambda x: torch.linalg.norm(x, ord=2)
+    dist_func = lambda x: linalg.norm(x, ord=2)
     cov_kwards = {
         'kernel_size': 3,
-        'lengthscale_init': 2,
-        'precision_init': 2,
+        'lengthscale_init': 1,
+        'precision_init': 1,
         'dist_func': dist_func,
         }
     cov_func = RadialBasisFuncCov(**cov_kwards)
-    mean = torch.zeros((9, 9))
-    p = MultiVariateGaussianPrior(mean, cov_func)
-    samples = p.sample(shape=(32, 3))
+    p = MultiVariateGaussianPrior(cov_func)
+    samples = p.sample(shape=[32, 3])
     print(samples.size())
