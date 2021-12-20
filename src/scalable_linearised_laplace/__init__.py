36d9516ad311e325a2982dddd51b9a1d5e961836
Johannes Leuschner <jleuschn@uni-bremen.de>
2021-12-21 00:28:27 +0100

    remove import

--- src/scalable_linearised_laplace/__init__.py
+++ src/scalable_linearised_laplace/__init__.py
@@ -1,3 +1,3 @@
 from .closures import vec_weight_prior_cov_mul
 from .utils_batch_jac import *
-from .batch_jac import vec_jac_mul_batch, vec_jac_mul_batch_cheker
+from .batch_jac import vec_jac_mul_batch
